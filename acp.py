14075840ccb8bc96c63061b761f63f378ca82fc6
Nur-A-Sabah <sajedurkabir@gmail.com>
2025-02-13 00:46:46 -0800

    acp

---
+++ acp.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.Screen( ).bgcolor("light blue")
+turtle.Screen( ).setup(300,400)
+ 
+side = turtle.Turtle( )
+
+side.forward(70)
+side.right(90)
+
+side.forward(70)
+side.right(90)
+
+side.forward(70)
+side.right(90)
+
+side.forward(70)
+side.right(90)
+
+turtle.done( )
